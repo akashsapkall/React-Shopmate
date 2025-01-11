b3e19714854655477585fc3cfd58ebde1173e13f
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-11 23:54:01 +0530

    update product card and product search

--- src/pages/ProductSearch.jsx
+++ src/pages/ProductSearch.jsx
@@ -1,19 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import { ProductCard } from "../components/ProductCard";
+
 export const ProductSearch = () => {
-  const [url, setUrl] = useState("http://localhost:8000/products");
+  const [url] = useState("http://localhost:8000/products");
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchParams] = useSearchParams();
-  const squery = searchParams.get("q")||"";
-  console.log(squery);
-  const { data:products}=useFetch(`${url}?name_like=${squery}`);
-  console.log(products);
+  const squery = searchParams.get("q") || "";
+  const { data: products } = useFetch(url);
+
+  useEffect(() => {
+    function sort() {
+      if (products) {
+        const filteredArray = products.filter((product) =>
+          product.name.toLowerCase().includes(squery.toLowerCase())
+        );
+        setFilteredProducts(filteredArray);
+      }
+    }
+    sort();
+  }, [products, squery]);
+
+  console.log(filteredProducts);
+
   return (
     <main className="dark:bg-gray-700">
       <div className="flex justify-center flex-wrap">
-        {products &&
-          products.map((product) => (
+        {filteredProducts &&
+          filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
       </div>
