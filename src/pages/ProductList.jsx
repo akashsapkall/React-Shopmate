2fe3cb67be067c0c770aae9218bd7a8a7a5a211d
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-11 18:17:52 +0530

    add product list as it is home page

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from "react"
+import { useFetch } from "../hooks/useFetch";
 
+import { ProductCard } from "../components/ProductCard";
 export const ProductList=()=>{
+    const [url,setUrl]=useState("http://localhost:8000/products");
+    const { data:products }=useFetch(url);
     return (
-        <>
-        <h1>List</h1>
-        </>
+        <main className="dark:bg-gray-700">
+            <div className="flex justify-center flex-wrap">
+                {products && products.map((product)=>(
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </main>
     )
 }
