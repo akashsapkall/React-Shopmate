0b5fb7841e117f5b40b23fa760156732af5089e8
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-12 01:03:48 +0530

    use useTitle to set Dynamic Title

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,7 @@
+import { useTitle } from "../hooks/useTitle"
 
 export const Cart=()=>{
+    useTitle("Cart");
     return (
         <>
         <h1>Cart</h1>
--- src/pages/ProductSearch.jsx
+++ src/pages/ProductSearch.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import { ProductCard } from "../components/ProductCard";
+import { useTitle } from "../hooks/useTitle";
 
 export const ProductSearch = () => {
   const [url] = useState("http://localhost:8000/products");
@@ -22,10 +23,12 @@ export const ProductSearch = () => {
     sort();
   }, [products, squery]);
 
-  console.log(filteredProducts);
-
+  useTitle(`Search Results For '${squery}'`);
   return (
     <main className="dark:bg-gray-700">
+      <div className="w-4/5 py-4 mx-auto text-2xl text-gray-700 dark:text-white">
+        <span className="">{(filteredProducts.length>0)?`Search Results For '${squery}'`:`No Search Results Found For '${squery}'`}</span>
+      </div>
       <div className="flex justify-center flex-wrap">
         {filteredProducts &&
           filteredProducts.map((product) => (
